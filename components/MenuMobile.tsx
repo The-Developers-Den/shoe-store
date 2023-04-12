3e457e457f8377a489b0c3c93e6ee3a178f0c9ac
Pratham <prathamsinghal2003@gmail.com>
2023-04-12 17:02:28 +0530

    feat: add footer

--- components/MenuMobile.tsx
+++ components/MenuMobile.tsx
@@ -2,45 +2,41 @@ import React from "react";
 import Link from "next/link";
 import { BsChevronDown } from "react-icons/bs";
 
-const data: { id: number; name: string; url?: string; subMenu?: boolean }[] = [
+const data = [
   { id: 1, name: "Home", url: "/" },
   { id: 2, name: "About", url: "/about" },
   { id: 3, name: "Categories", subMenu: true },
   { id: 4, name: "Contact", url: "/contact" },
 ];
 
-const subMenuData: { id: number; name: string; doc_count?: number }[] = [
-  { id: 1, name: "Jordan", doc_count: 11 },
-  { id: 2, name: "Sneakers", doc_count: 8 },
-  { id: 3, name: "Running shoes", doc_count: 64 },
-  { id: 4, name: "Football shoes", doc_count: 107 },
-];
-
-const Menu = ({
+const MenuMobile = ({
   showCatMenu,
   setShowCatMenu,
+  setMobileMenu,
   categories,
 }: {
   showCatMenu: boolean;
   setShowCatMenu: (value: boolean) => void;
+  setMobileMenu: (value: boolean) => void;
   categories: any;
 }) => {
   return (
-    <ul className="hidden md:flex items-center gap-8 font-medium text-black">
+    <ul className="flex flex-col md:hidden font-bold absolute top-[50px] left-0 w-full h-[calc(100vh-50px)] bg-white border-t text-black">
       {data.map((item) => {
         return (
           <React.Fragment key={item.id}>
-            {item?.subMenu ? (
+            {!!item?.subMenu ? (
               <li
-                className="cursor-pointer flex items-center gap-2 relative"
-                onMouseEnter={() => setShowCatMenu(true)}
-                onMouseLeave={() => setShowCatMenu(false)}
+                className="cursor-pointer py-4 px-5 border-b flex flex-col relative"
+                onClick={() => setShowCatMenu(!showCatMenu)}
               >
-                {item.name}
-                <BsChevronDown size={14} />
+                <div className="flex justify-between items-center">
+                  {item.name}
+                  <BsChevronDown size={14} />
+                </div>
 
                 {showCatMenu && (
-                  <ul className="bg-white absolute top-6 left-0 min-w-[250px] px-1 py-1 text-black shadow-lg">
+                  <ul className="bg-black/[0.05] -mx-5 mt-4 -mb-4">
                     {categories?.map(
                       ({
                         attributes: c,
@@ -58,14 +54,17 @@ const Menu = ({
                             key={id}
                             href={`/category/`}
                             // href={`/category/${c.slug}`}
-                            onClick={() => setShowCatMenu(false)}
+                            onClick={() => {
+                              setShowCatMenu(false);
+                              setMobileMenu(false);
+                            }}
                           >
-                            <li className="h-12 flex justify-between items-center px-3 hover:bg-black/[0.03] rounded-md">
+                            <li className="py-4 px-8 border-t flex justify-between">
                               {/* {c.name}  */}
-                              Hola amigo
+                              Jordans
                               <span className="opacity-50 text-sm">
                                 {/* {`(${c.products.data.length})`} */}
-                                12
+                                20
                               </span>
                             </li>
                           </Link>
@@ -76,8 +75,10 @@ const Menu = ({
                 )}
               </li>
             ) : (
-              <li className="cursor-pointer">
-                <Link href={item?.url!}>{item.name}</Link>
+              <li className="py-4 px-5 border-b">
+                <Link href={item?.url!} onClick={() => setMobileMenu(false)}>
+                  {item.name}
+                </Link>
               </li>
             )}
           </React.Fragment>
@@ -87,4 +88,4 @@ const Menu = ({
   );
 };
 
-export default Menu;
+export default MenuMobile;
